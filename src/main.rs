8178143e103db37eee84e484ec904159a9d91745
ysk8hori <ysk8.hori@gmail.com>
2021-05-23 23:43:21 +0900

    ゲーム完成

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guess {}", guess);
+        println!("You guess {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small."),
+            Ordering::Greater => println!("Too big."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
